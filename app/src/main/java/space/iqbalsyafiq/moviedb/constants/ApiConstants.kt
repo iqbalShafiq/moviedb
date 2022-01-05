0c984a894901a02bcf57a8a85417f0a695428d4f
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2022-01-05 17:46:18 +0700

    Initial Commit

---
+++ app/src/main/java/space/iqbalsyafiq/moviedb/constants/ApiConstants.kt
@@ -0,0 +1,8 @@
+package space.iqbalsyafiq.moviedb.constants
+
+class ApiConstants {
+    companion object {
+        const val API_KEY = "ea4695c99ce20bae9e18aec65dfe1498"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
+}
---
+++ app/src/main/java/space/iqbalsyafiq/moviedb/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package space.iqbalsyafiq.moviedb.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import space.iqbalsyafiq.moviedb.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
